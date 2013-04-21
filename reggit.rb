f2242fdaecf1dfc9597b3ea9e8cc3314aa804e99
em <eugen.mayer@kontextwork.de>
2013-04-21 17:57:15 +0200

    fixing split usage, as an array is returned

--- reggit.rb
+++ reggit.rb
@@ -21,6 +21,7 @@ Find.find(@searchpath) do |path|
     folder = File.basename(path)
     if folder == ".git"
       git_root = String.new(path).split("/.git")
+      git_root = git_root[0]
       Git_roots.push(git_root)
     end
   end
@@ -32,7 +33,6 @@ xmlfile.close
 
 #thats were all the git-folder mappings are stored in the XML
 xml_git_root = xmldoc.elements["/project/component"]
-
 
 # TODO: Maybe rather compare with the current Git_roots list and exclude existent, rather then delting all existent
 # TODO: otherwise, maybe rather delete the component node and recreate it
